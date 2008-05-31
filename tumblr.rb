64753c37d26497b9bd6d8e662e27ac26c38127a7
Amos King <amos.l.king@gmail.com>
2008-05-31 09:59:23 -0700

    initial commit

---
+++ tumblr.rb
@@ -0,0 +1,40 @@
+require "rubygems"
+require "sinatra"
+require "active_record"
+
+ActiveRecord::Base.establish_connection(
+   :adapter  => "sqlite3",
+   :database => "tumblr.sqlite3"
+ )
+
+class Migrate < ActiveRecord::Migration
+	def self.up
+		create_table :posts do |t|
+			t.text :body
+			t.string :title
+		end
+	end
+	
+	def self.down
+		drop_table :posts
+	end
+end
+
+class Post < ActiveRecord::Base
+end
+
+Migrate.up unless Post.table_exists?
+
+get '/' do
+	@posts = Post.find(:all)
+	erb :index
+end
+
+post '/post' do
+		until Post.count(:all) <= 5
+			Post.delete(Post.find(:first))
+		end
+		@post = Post.new(:title => params[:title], :body => params[:body])
+		@post.save
+		redirect '/'
+end
